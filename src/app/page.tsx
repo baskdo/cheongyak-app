79e97c67cd182cf09cca6bca0b9d7ab5fd3393a7
baskdo <s.baskdo@gmail.com>
2026-04-25 07:42:35 +0900

    경쟁률 카드 부가설명 글자 축소

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -422,14 +422,14 @@ function CompetitionCard({ item }: { item: CompetitionItem }) {
           특공 (공급 {specialTotalSupply.toLocaleString()}){' '}
           <span className="font-bold text-blue-600">{specialTotalReq.toLocaleString()}건</span>
           <span className="ml-1">접수</span>
-          <span className="text-gray-400 ml-1">(기관/이전 제외)</span>
+          <span className="text-gray-400 ml-1 text-[10px]">(기관/이전 제외)</span>
         </div>
 
         <div>
           1순위 (공급 {rank1Supply.toLocaleString()}){' '}
           <span className="font-bold text-red-500">{rank1TotalReq.toLocaleString()}건</span>
           <span className="ml-1">접수</span>
-          <span className="text-gray-400 ml-1">(해당/기타 합계)</span>
+          <span className="text-gray-400 ml-1 text-[10px]">(해당/기타 합계)</span>
         </div>
 
         <div className="text-gray-500 text-xs">
