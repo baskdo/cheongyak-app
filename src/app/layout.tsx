7aba3391eb2dc11048bb570f10a793e7c6bf71c6
baskdo <s.baskdo@gmail.com>
2026-04-27 11:47:31 +0900

    브라우저 탭 제목 변경

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,8 +2,8 @@ import type { Metadata } from 'next'
 import './globals.css'
 
 export const metadata: Metadata = {
-  title: '청약홈 간략조회_MarU',
-  description: '복잡한 아파트 청약 공고, 요약된 정보로 쉽고 빠르게 확인하세요.',
+  title: '청약홈 요약_MarU',
+  description: '아파트 청약 공고, 요약된 정보로 빠르게 확인하세요.',
 }
 
 export default function RootLayout({
